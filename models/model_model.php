f394f9272ac251e679bf2c3a410510268b70c766
JohnOscar-Mendoza <smartwaveoscar@Smartwave14.local>
2015-07-15 18:14:33 +0800

    Migrate to Codeigniter. Done with add without stylesheet

---
+++ models/model_model.php
@@ -0,0 +1,12 @@
+<?php
+
+class Models_model extends CI_Model()
+{
+
+	public function add()
+	{
+		return 1 + 1;
+	}
+}
+
+?>
---
+++ views/add.php
@@ -0,0 +1,21 @@
+//ADD ACTION
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php print $modelId; ?>
+	<?php print $modelName; ?>
+<form>
+	<label for="modelName">Name:</label><input type="text" name="modelName" />
+	<label for="age">Age:</label><input type="text" name="age" />
+	<label for="address">Address:</label><input type="text" name="address" />
+	<label for="fbLink">Facebook:</label><input type="text" name="fbLink" />
+	<label for="twitterLink">Twitter:</label><input type="text" name="twitterLink" />
+	<label for="instagramLink">Intagram:</label><input type="text" name="instagramLink" />
+	<label for="imageUpload">Image:</label><input type="file" name="imageUpload" />
+	
+</form>
+
+</body>
+</html>
