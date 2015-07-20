45eb2406577685aa21a211c9793023e4113e734b
JohnOscar-Mendoza <smartwaveoscar@Smartwave14.local>
2015-07-20 18:52:39 +0800

    modeified api

--- application/controllers/api/ModelApi.php
+++ application/controllers/api/ModelApi.php
@@ -1,4 +1,5 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php 
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 class ModelApi extends REST_Controller {
 
@@ -26,37 +27,18 @@ class ModelApi extends REST_Controller {
 	{
 		
 		$this->load->model('api/modelapi_model');
-		//$this->modelapi_model->view();
 		$this->response($this->modelapi_model->view(), 200);
 	}
 	public function add_post()
 	{
-		/*$data['name'] = $this->post('name');
-		$data['age'] = $this->post('age');
-		$data['address'] = $this->post('address');
-		$data['fbLink'] = $this->post('fbLink');
-		$data['twitterLink'] = $this->post('twitterLink');
-		$data['instagramLink'] = $this->post('instagramLink');
-		$data['image'] = $this->post('image');
-		*/
+		
 		$this->load->model('api/modelapi_model');
-		//$data = array();
-		//$data = $_POST;
 		$this->modelapi_model->add($this->post());
 		
 		$this->response($this->post(), 201);
 	}
 	public function edit_put($id)
 	{
-		/*
-		$data['name'] = $this->put('name');
-		$data['age'] = $this->put('age');
-		$data['address'] = $this->put('address');
-		$data['fbLink'] = $this->put('fbLink');
-		$data['twitterLink'] = $this->put('twitterLink');
-		$data['instagramLink'] = $this->put('instagramLink');
-		$data['image'] = $this->post('image');
-		*/
 		$this->load->model('api/modelapi_model');
 		$this->modelapi_model->update($this->put(), $id);
 		$this->response([
